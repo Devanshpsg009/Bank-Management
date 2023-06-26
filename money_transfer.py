2d577d9921f8804ca83f9faa0166079b212d50c7
Devanshpsg009 <devanshpsg009@gmail.com>
2023-06-26 21:57:29 +0530

    Done

---
+++ money_transfer.py
@@ -0,0 +1,17 @@
+import tkinter,customtkinter
+from PIL import Image,ImageTk
+
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("green")
+root = customtkinter.CTk()
+root.title("Account Management")
+root.geometry("800x600+300+75")
+img = Image.open("C:\\Users\\laxmi\\Desktop\\My Coding\\school project\\Bank-Management\\background.jpg")
+img = img.resize((1366, 768))
+img1 = ImageTk.PhotoImage(img)
+l1 = customtkinter.CTkLabel(master=root, image=img1,text="")
+l1.image = img1
+l1.pack()
+frame1 = customtkinter.CTkFrame(master=l1, corner_radius=15, width=320, height=400, bg_color="black")
+frame1.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+root.mainloop()
